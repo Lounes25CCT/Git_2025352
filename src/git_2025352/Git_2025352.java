988028ef5c6582c97600b5b0d40c5bf761a38182
Lounes25CCT <lounes.ismal@gmail.com>
2025-12-05 14:36:53 +0000

    Second commit - no code

--- src/git_2025352/Git_2025352.java
+++ src/git_2025352/Git_2025352.java
@@ -15,7 +15,7 @@ public class Git_2025352 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        System.out.println("Louanes_2025352");
     }
     
 }
